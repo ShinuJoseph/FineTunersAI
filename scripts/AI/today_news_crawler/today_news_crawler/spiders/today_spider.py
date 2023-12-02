b8a73b60e1cc6c3a2fa9df0d33d0b16ffa4235b6
‘Akshay-Rajesh’ <akshay.rjsh@gmail.com>
2023-12-02 12:52:57 +0100

    akshay- adding a new output file with line break , modified the code accordingly

--- scripts/AI/today_news_crawler/today_news_crawler/spiders/today_spider.py
+++ scripts/AI/today_news_crawler/today_news_crawler/spiders/today_spider.py
@@ -1,21 +1,32 @@
-# importing the necessary libraries.
+# Importing the necessary libraries.
 import scrapy
 
-# creating the spider class.
+# Creating the spider class.
 class TodaySpider(scrapy.Spider):
+    # Setting the spider name and initial URL.
     name = 'today_spider'
-    start_urls = ['https://www.today.com/archive/articles/2023/november']
+    start_urls = ['https://www.today.com/archive/articles/2013/december']
 
-    # parsing the main page.
+    # Parsing the main page.
     def parse(self, response):
+        # Extracting the main section of the page.
         main = response.xpath("//main[@class='MonthPage']")
+        
+        # Iterating through the links in the main section.
         for a in main.xpath(".//a"):
+            # Extracting link and title from each link.
             link = a.xpath("./@href").get()
             title = a.xpath("./text()").get()
+            
+            # Making a request to the link and calling parse_link function with additional metadata.
             yield scrapy.Request(url=link, callback=self.parse_link, meta={'title': title})
 
+    # Parsing the individual link page.
     def parse_link(self, response):
-        content = response.xpath("//p/text()").getall()
+        # Extracting the content from the link page.
+        content = response.xpath("//p").getall()
+        
+        # Yielding a dictionary containing link, title, and content.
         yield {
             "link": response.url,
             "title": response.meta['title'],
